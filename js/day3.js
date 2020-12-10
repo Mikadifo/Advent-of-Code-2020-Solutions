7cc54506b32538351a3c9bd55b21f209a14f089f
Michael <michaelpadillaheredia@hotmail.com>
2020-12-10 10:46:10 -0500

    day3 part2

--- js/day3.js
+++ js/day3.js
@@ -1,15 +1,15 @@
 import { map } from './day3Input.js'
 
 const mapArray = map.split('\n')
-mapArray.shift()
 
-let tress = 0
+let trees = 0
 let position = 3
 let currentSquare = 0
 let missingTimes = 1
 
 const TREE = '#'
-const cut = () => tress ++
+
+mapArray.shift()
 
 mapArray.forEach(currentLine => {
     currentSquare = currentLine[position]
@@ -20,10 +20,40 @@ mapArray.forEach(currentLine => {
 	currentSquare = currentLine[position]
     }
 
-    if (currentSquare === TREE) cut()
+    if (currentSquare === TREE) trees ++
 
     position += 3
 })
 
-console.log(tress)
+//console.log(trees)
+
+const moveInMap = (right, down) => {
+    let skipTimes = down
+    position = 0
+    trees = 0
+    currentSquare = 0
+
+    for (let currentLine of mapArray) {
+	if (skipTimes > 1) {
+	    skipTimes --
+	    continue
+	}
+	skipTimes = down
+	position += right
+
+	currentSquare = currentLine[position % currentLine.length]
+
+	if (currentSquare === TREE) trees ++
+    }
+
+    return trees
+}
+
+trees = moveInMap(1, 1) * //84
+	moveInMap(3, 1) * //228
+	moveInMap(5, 1) * //89
+	moveInMap(7, 1) * // 100
+	moveInMap(1, 2) //40
+
+console.log(trees)
 
